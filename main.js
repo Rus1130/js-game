748f9616a5208b213f3eb80d74501b0b0f33580a
Rus1130 <arirogozi@gmail.com>
2022-12-11 00:36:23 -0500

    commit

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const { app, BrowserWindow } = require('electron');
 
 app.on('ready', function(){
 
-    var win = new BrowserWindow({ width: 600, height: 600 });
+    var win = new BrowserWindow({ width: 1000, height: 1000 });
 
     win.loadFile('index.html')
 });
